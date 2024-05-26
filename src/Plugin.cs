3a0e1970087b3948393082f5b5246faf00effad2
ddemile <emilienhoarau10@gmail.com>
2024-05-26 09:02:16 +0400

    first commit

---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace FakeAchievements
+{
+    [BepInPlugin(MOD_ID, "Fake Achievements", "0.1.0")]
+    class Plugin : BaseUnityPlugin
+    {
+        private const string MOD_ID = "ddemile.fake_achievements";
+
+        // Add hooks
+        public void OnEnable()
+        {
+            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+
+            // Put your custom hooks here!
+        }
+        
+        // Load any resources, such as sprites or sounds
+        private void LoadResources(RainWorld rainWorld)
+        {
+        }
+    }
+}
